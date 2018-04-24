185d815ef52af5f52f62ce7d41d274a8df83938f
Oghenetega <okeremeta.tega@gmail.com>
2018-04-24 11:07:13 +0100

    Add seedData for dummy API endpoints

---
+++ server/seedData/dummyOrders.js
@@ -0,0 +1,24 @@
+const orders = [
+  {
+    id: 1,
+    meals: [],
+  },
+  {
+    id: 2,
+    meals: [],
+  },
+  {
+    id: 3,
+    meals: [],
+  },
+  {
+    id: 4,
+    meals: [],
+  },
+  {
+    id: 5,
+    meals: [],
+  },
+];
+
+export default orders;
