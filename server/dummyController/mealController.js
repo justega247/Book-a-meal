2a06a80f38e21cbfeb6853c789f894fcb96c305a
Oghenetega <okeremeta.tega@gmail.com>
2018-04-25 13:29:44 +0100

    feature remove unused codes [Fixes #156810878]

--- server/dummyController/mealController.js
+++ server/dummyController/mealController.js
@@ -1,6 +1,5 @@
 import meals from '../seedData/dummyMeal';
 
-
 /**
  * @class Meals
  */
@@ -41,32 +40,3 @@ class Meals {
 }
 
 export default Meals;
-
-// exports.postMeal = (req, res) => {
-//   for(let i = 0; i < meals.length; i += 1) {
-//     if (req.body.name.trim() === '' ||
-//        req.body.name.trim() === meals[i].name) {
-//       return res.status(400)
-//         .json({
-//           message: 'Your meal name is invalid'
-//         });
-//     } else if (req.body.category.trim() === '') {
-//       return res.status(400)
-//         .json({
-//           message: 'Your meal category cannot be empty'
-//         });
-//     }
-//   }
-//   meals.push({
-//     mealId: meals.length + 1,
-//     name: req.body.name,
-//     category: req.body.category,
-//     image: req.body.image,
-//     price: req.body.price
-//   });
-//   return res.status(200)
-//     .json({
-//       message: 'A new meal was added',
-//       details: meals[meals.length - 1]
-//     })
-// };
