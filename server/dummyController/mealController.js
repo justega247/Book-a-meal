a5618846fc59c1166b8129afd95b510978cde73e
Oghenetega <okeremeta.tega@gmail.com>
2018-05-01 18:26:40 +0100

    Add API endpoint for deleting a meal option

--- server/dummyController/mealController.js
+++ server/dummyController/mealController.js
@@ -96,6 +96,30 @@ class Meals {
       message: 'Sorry,no meal with that id exists'
     });
   }
+
+  /**
+ * @return {Object} leftover meals
+ * @param {param} req
+ * @param {param} res
+ */
+  static removeMeal(req, res) {
+
+    const mealId = parseInt(req.params.mealId);
+    const findMealWithId = meal => meal.mealId === mealId;
+    const index = meals.findIndex(findMealWithId);
+
+    if (index === -1) {
+      return res.status(404).json({
+        message: 'Sorry,there is no meal with that mealId'
+      });
+    }
+    meals.splice(index, 1);
+
+    return res.status(200).json({
+      message: 'A meal was just deleted',
+      leftoverMeals: meals
+    });
+  }
 }
 
 export default Meals;
