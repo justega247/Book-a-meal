44730b1322a27beb3983926892557a4c17e30d52
Oghenetega <okeremeta.tega@gmail.com>
2018-05-01 18:58:05 +0100

    Add API endpoint to set the menu for the day

--- server/dummyRoutes/menuRoutes.js
+++ server/dummyRoutes/menuRoutes.js
@@ -1,5 +1,9 @@
 import express from 'express';
 
+import Menu from '../dummyController/menuController';
+
 const router = express.Router();
+
+router.post('/', Menu.addMenu);
 
 module.exports = router;
