ca7d7b1fe53c045dfc2ae1a4f09c6eb23ba0a1c3
Oghenetega <okeremeta.tega@gmail.com>
2018-05-01 10:27:44 +0100

    Setup API endpoint for adding a new meal

--- server/dummyController/mealController.js
+++ server/dummyController/mealController.js
@@ -24,6 +24,40 @@ class Meals {
     }
     return res.status(500);
   }
+
+  /**
+ * @return {Object} added meal
+ * @param {param} req
+ * @param {param} res
+ */
+  static addMeal(req, res) {
+    for (let i = 0; i < meals.length; i += 1) {
+      if (req.body.name.trim() === '' ||
+       req.body.name.trim() === meals[i].name) {
+        return res.status(400)
+          .json({
+            message: 'Sorry,that meal name is invalid'
+          });
+      } else if (req.body.category.trim() === '') {
+        return res.status(400)
+          .json({
+          message: 'Sorry,meal category cannot be empty'
+          });
+      }
+    }
+    meals.push({
+      mealId: meals.length + 1,
+      name: req.body.name,
+      category: req.body.category,
+      image: req.body.image,
+      price: req.body.price
+    });
+    return res.status(201)
+      .json({
+        message: 'Success',
+        details: meals[meals.length - 1]
+      });
+  }
 }
 
 export default Meals;
--- server/dummyRoutes/mealRoutes.js
+++ server/dummyRoutes/mealRoutes.js
@@ -5,5 +5,6 @@ import Meals from '../dummyController/mealController';
 const router = express.Router();
 
 router.get('/', Meals.mealsAvailable);
+router.post('/', Meals.addMeal);
 
 module.exports = router;
