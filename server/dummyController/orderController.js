b1f4f7a0be3b6d3b6d18293ea21aae83db8bb60c
Oghenetega <okeremeta.tega@gmail.com>
2018-05-02 14:41:31 +0100

    Add price calculation to endpoint

--- server/dummyController/orderController.js
+++ server/dummyController/orderController.js
@@ -16,9 +16,24 @@ class Orders {
         orders: []
       });
     } else if (orders.length > 0) {
+      let total;
+      let cost = [];
+
+      for(let i = 0; i < orders.length; i += 1) {
+        for(let j = 0; j < orders[i].meals.length; j += 1) {
+          cost.push(orders[i].meals[j].price);
+        }
+      }
+
+      const reducer = (acc, currentValue) => acc + currentValue;
+      total = cost.reduce(reducer);
+      console.log(total);
+
       const ordersAvailable = orders.map(order => Object.assign({}, order));
+
       return res.status(200).json({
         message: 'Here are the orders for the day',
+        total: total,
         orders: ordersAvailable
       });
     }
