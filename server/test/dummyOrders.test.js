71f6af7c28e52f2d1d348b4d0a877bffb3e39ffa
Oghenetega <okeremeta.tega@gmail.com>
2018-05-02 16:26:18 +0100

    Setupt API endpoint for getting all the orders of the day

--- server/test/dummyOrders.test.js
+++ server/test/dummyOrders.test.js
@@ -81,3 +81,38 @@ describe('PUT /', () => {
       .end(done);
   });
 });
+
+describe('GET /', () => {
+
+  it('should return an array of all the orders made', (done) => {
+
+    request(app)
+      .get('/api/v1/orders/')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.message).to.equal('Here are the orders for the day');
+        expect(res.body.orders).to.be.an('array');
+        expect(res.body.orders.length).to.equal(orders.length);
+      })
+      .end(done);
+  })
+
+  describe('#when no order has been made', () => {
+    before(() => {
+      orders.splice(0);
+    })
+
+    it('should return an empty array when no orders have been made',
+      (done) => {
+
+        request(app)
+          .get('/api/v1/orders/')
+          .expect(200)
+          .expect((res) => {
+            expect(res.body.message).to.equal('Oh! no orders made yet');
+            expect(res.body.orders.length).to.equal(0);
+          })
+          .end(done);
+      })
+  })
+});
