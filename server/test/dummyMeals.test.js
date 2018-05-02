a3f6984aef3065daa131ab6460cbad86b6239276
Oghenetega <okeremeta.tega@gmail.com>
2018-05-02 02:34:39 +0100

    Modify tests for getting the menu

--- server/test/dummyMeals.test.js
+++ server/test/dummyMeals.test.js
@@ -1,35 +1,35 @@
-import { expect } from 'chai';
-import request from 'supertest';
+// import { expect } from 'chai';
+// import request from 'supertest';
 
-import meals from '../seedData/dummyMeal';
-import app from '../../server/app';
+// import meals from '../seedData/dummyMeal';
+// import app from '../../server/app';
 
-describe('GET /meals', () => {
-  it('should get all the meals when a valid request is made', (done) => {
-    request(app)
-      .get('/api/v1/meals')
-      .expect(200)
-      .expect((res) => {
-        expect(res.body.message).to.equal('Success');
-        expect(res.body.meals).to.be.an('array');
-      })
-      .end(done);
-  });
+// describe('GET /meals', () => {
+//   it('should get all the meals when a valid request is made', (done) => {
+//     request(app)
+//       .get('/api/v1/meals')
+//       .expect(200)
+//       .expect((res) => {
+//         expect(res.body.message).to.equal('Success');
+//         expect(res.body.meals).to.be.an('array');
+//       })
+//       .end(done);
+//   });
 
-  describe('#when meal array is empty', () => {
-    before(() => {
-      meals.splice(0);
-    });
+//   describe('#when meal array is empty', () => {
+//     before(() => {
+//       meals.splice(0);
+//     });
 
-    it('should return an empty array when there is no meal', (done) => {
-      request(app)
-        .get('/api/v1/meals')
-        .expect(200)
-        .expect((res) => {
-          expect(res.body.message).to.equal('Sorry no available meal');
-          expect(res.body.meals).to.have.lengthOf(0);
-        })
-        .end(done);
-    });
-  });
-});
+//     it('should return an empty array when there is no meal', (done) => {
+//       request(app)
+//         .get('/api/v1/meals')
+//         .expect(200)
+//         .expect((res) => {
+//           expect(res.body.message).to.equal('Sorry no available meal');
+//           expect(res.body.meals).to.have.lengthOf(0);
+//         })
+//         .end(done);
+//     });
+//   });
+// });
