261b4ca79e88eb2337d9c0685f2b65230a497144
Oghenetega <okeremeta.tega@gmail.com>
2018-05-02 15:55:45 +0100

    Add API enpoint for modifying an order

--- server/test/dummyOrders.test.js
+++ server/test/dummyOrders.test.js
@@ -2,7 +2,9 @@ import { expect } from 'chai';
 import request from 'supertest';
 
 import app from '../../server/app';
+import meals from '../seedData/dummyMeal';
 import orders from '../seedData/dummyOrders';
+import testMeals from '../seedData/testMeals'
 
 describe('POST /', () => {
   it('should add a new order to the orders list', (done) => {
@@ -34,6 +36,47 @@ describe('POST /', () => {
         expect(res.body.message).to
           .equal('You have not specified any meal to order');
         expect(res.body.yourOrder).to.deep.not.equal(orders[orders.length - 1]);
+      })
+      .end(done);
+  });
+});
+
+describe('PUT /', () => {
+  before(() => {
+    meals.splice(0);
+    meals.push(...testMeals);
+  });
+
+  it('should return a modified order if valid data is sent', (done) => {
+    let changeOrder = {
+      addOrder: [1, 3, 7],
+      deleteOrder: [2, 4]
+    };
+
+    request(app)
+      .put('/api/v1/orders/2')
+      .send(changeOrder)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.message).to.equal('Your order has been modified');
+        expect(res.body.yourNewOrder.meals).deep.to.include(meals[0]);
+        expect(res.body.yourNewOrder.meals).deep.to.not.include(meals[1]);
+      })
+      .end(done);
+  });
+
+  it('should throw an error for an invalid orderId', (done) => {
+    let changeOrder = {
+      addOrder: [1, 3, 7],
+      deleteOrder: [2, 4]
+    };
+
+    request(app)
+      .put('/api/v1/orders/22')
+      .send(changeOrder)
+      .expect(400)
+      .expect((res) => {
+        expect(res.body.message).to.equal('Your orderId is invalid');
       })
       .end(done);
   });
