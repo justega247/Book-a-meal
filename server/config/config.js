357c2bead15fedc409b960b21532c8e2868328a3
Oghenetega <okeremeta.tega@gmail.com>
2018-05-11 07:27:11 +0100

    Update production config variable

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ module.exports = {
     dialect: process.env.DIALECT
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: process.env.DATABASE_URL,
     dialect: 'postgres'
   }
 };
